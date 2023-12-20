41c459ac20ad4a56ed0d14b9919a1e89efa192c6
Serhii-Kuzmin <seryy8248@gmail.com>
2023-12-20 13:01:59 +0100

    bagfixed

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 
 import { PersistGate } from 'redux-persist/integration/react';
-// import { persistor, store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
